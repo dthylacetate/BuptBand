f6fa667b2639424eb6159d326e7c3d0b4a443209
dthylacetate <16567689+dthylacetate@user.noreply.gitee.com>
2026-04-08 13:41:18 +0800

    feat: 成功初始化 BuptBand 后端骨架 (Spring Boot 3 + JPA + H2)

---
+++ src/main/java/com/bupt/BuptBand/BuptBandApplication.java
@@ -0,0 +1,13 @@
+package com.bupt.BuptBand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuptBandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuptBandApplication.class, args);
+	}
+
+}
